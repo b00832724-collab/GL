bfbc9318e1dc00d0508a167ad36517a22f20cdc2
Loichris <loic.lalande@kedgebs.com>
2025-10-16 13:52:35 +0200

    Update configuration for new repository

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/greenlink-vitis-watch/",
+  base: "/GL/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
